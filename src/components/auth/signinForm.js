d5d08b1639d91d832b81293c70cbbdbb07a365e6
costajosh47 <59314891+costajosh47@users.noreply.github.com>
2021-04-21 19:21:25 -0400

    styled sign in form and applied grid

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,38 +1,35 @@
 import React, { Component } from 'react';
-                                           
+
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
 
 class SignInForm extends Component {
-    render () {
+    render() {
         const { className, handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-                <Field className='sign-in-form__email' 
-                type='email' 
-                title='Email' 
-                placeholder='Email' 
-                name='email' 
-                component={FormInput} 
-                />
-                <Field className='sign-in-form__email' 
-                type='password' 
-                title='Password' 
-                placeholder='Password' 
-                name='password' 
-                component={FormInput} 
-                />
+                <Field className='sign-in-form__email'
+                type='email'
+                title='Email'
+                placeholder='Email'
+                name='email'
+                component={FormInput}/>
+                <Field className='sign-in-form__password'
+                type='password'
+                title='Password'
+                placeholder='Password'
+                name='password'
+                component={FormInput}/>
                 <div className='sign-in-form__line'></div>
-                <Field className='sign-in-form__email' 
-                onClick={() => console.log('tryna sumbit')}
-                type='submit' 
-                title='Login'  
-                name='login' 
-                component={FormButton} 
-                />
+                <Field className='sign-in-form__login'
+                onClick={() => console.log('tryna submit')}
+                type='submit'
+                title='Login'
+                name='login'
+                component={FormButton}/>
             </form>
-        );
+        )
     }
 }
 
